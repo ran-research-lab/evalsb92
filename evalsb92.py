46a34b43116958927ed9fdd0b980cc74e932f6c6
Rafael Arce Nazario <rafael.arce@upr.edu>
2020-03-06 17:28:14 -0400

    creo que arregle el desorden de los indices

--- evalsb92.py
+++ evalsb92.py
@@ -27,10 +27,6 @@ criteriosList = ['Asistencia', 'Puntualidad', 'Utilizacion Tiempo', 'Reposicion 
 
 
 
-
-
-
-
 def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
